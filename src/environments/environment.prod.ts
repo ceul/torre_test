dec24959557ef26f29996f74454511964cf8de48
ceul <ceul@ceul.ceul>
2020-11-17 09:40:02 -0500

    Change a confution betwen getuser and getjob

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
-  getUser: 'https://torre.co/api/opportunities/',
-  getJob: 'https://torre.bio/api/bios/',
+  production: true,
+  getJob: 'https://torre.co/api/opportunities/',
+  getUser: 'https://torre.bio/api/bios/',
   searchJobs: 'https://search.torre.co/opportunities/_search/?',
   searchPeople: 'https://search.torre.co/people/_search/?'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  getUser: 'https://torre.co/api/opportunities/',
-  getJob: 'https://torre.bio/api/bios/',
+  getJob: 'https://torre.co/api/opportunities/',
+  getUser: 'https://torre.bio/api/bios/',
   searchJobs: 'https://search.torre.co/opportunities/_search/?',
   searchPeople: 'https://search.torre.co/people/_search/?'
 };
